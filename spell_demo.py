bb45b936b10fce99808e7dcdc7087228b8fa4e03
AlexeyMalafeev <amalafeev@yandex.ru>
2023-02-26 13:28:01 +0300

    feat: add infinite loop to demo

--- spell_demo.py
+++ spell_demo.py
@@ -6,3 +6,8 @@ chkr = SimpleSpellchecker(print_steps=True)
 print(chkr.check('Ну что ты это каждый челвек должен знать!'))
 print(chkr.check('Мы шли по подъезду и стучались в каждую жверь, ожидая, что нам откроют.'))
 print(chkr.check('Есть такая поговорка: на ловца и жверь бежит.'))
+print()
+
+while sent := input('> '):
+    print(chkr.check(sent))
+    print()
